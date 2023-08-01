81c0b5390ddab6e269c8671a9d717f74fc7eec31
Cesar Trujillo <catgx55@gmail.com>
2023-08-01 08:07:29 -0500

    agregando funcion de agregarContacto

--- lista-contactos.js
+++ lista-contactos.js
@@ -1,63 +1,117 @@
-function agregar(persona){
-  console.log("Agregando contactos nuevo");
-  contacto.push(persona);
+function agregar(contacto) {
+  for (let i = 0; i < contacto.length; i++) {
+    contacto.push([i]);
+  }
+  console.log(
+    "Agregando contactos nuevo: " + "\n___________________________________"
+  );
 }
 
-function mostrar(){
-  for(let i = 0; i < contacto.length; i++){
+function eliminar(){
+  contacto.push(contacto);
+}
+
+
+function mostrar() {
+  for (let i = 0; i < contacto.length; i++) {
     console.log(contacto[i].datosCompletos);
   }
 }
 
-console.log('Bienvenidos a la lista de contactos' + '\n___________________________________')
-let contacto = [{
-  id: 1,
-  nombre: 'Cesar',
-  apellido: 'Trujillo',
-  telefono: 321654987,
-  ubicaciones: {
-    ciudad: 'Armenia',
-    direccion: 'Mz 4 Cs 144'
+console.log(
+  "Bienvenidos a la lista de contactos" +
+    "\n___________________________________"
+);
+let contacto = [
+  {
+    id: 1,
+    nombre: "Cesar",
+    apellido: "Trujillo",
+    telefono: 321654987,
+    ubicaciones: {
+      ciudad: "Armenia",
+      direccion: "Mz 4 Cs 144",
+    },
+    get datosCompletos() {
+      return (
+        "Contacto N°: " +
+        this.id +
+        "\nNombre: " +
+        this.nombre +
+        "\nApellido: " +
+        this.apellido +
+        "\nTelefono: " +
+        this.telefono +
+        "\nCiudad: " +
+        this.ubicaciones.ciudad +
+        "\nDireccion: " +
+        this.ubicaciones.direccion +
+        "\n___________________________________"
+      );
+    },  
   },
-   get datosCompletos(){
-     return 'Contacto N°: '+ this.id + '\nNombre: ' + this.nombre + '\nApellido: ' + this.apellido + 
-     '\nTelefono: ' + this.telefono + '\nCiudad: ' + this.ubicaciones.ciudad + '\nDireccion: ' + this.ubicaciones.direccion + '\n___________________________________';
-   }
-},
 
-{
-  id: 2,
-  nombre: 'Henry',
-  apellido: 'Trujillo',
-  telefono: 321654,
-  ubicaciones: {
-    ciudad: 'Armenia',
-    direccion: 'Mz 4 Cs 144'
+  {
+    id: 2,
+    nombre: "Henry",
+    apellido: "Trujillo",
+    telefono: 321654,
+    ubicaciones: {
+      ciudad: "Armenia",
+      direccion: "Mz 4 Cs 144",
+    },
+    get datosCompletos() {
+      return (
+        "Contacto N°: " +
+        this.id +
+        "\nNombre: " +
+        this.nombre +
+        "\nApellido: " +
+        this.apellido +
+        "\nTelefono: " +
+        this.telefono +
+        "\nCiudad: " +
+        this.ubicaciones.ciudad +
+        "\nDireccion: " +
+        this.ubicaciones.direccion +
+        "\n___________________________________"
+      );
+    },
   },
-   get datosCompletos(){
-     return 'Contacto N°: '+ this.id + '\nNombre: ' + this.nombre + '\nApellido: ' + this.apellido + 
-     '\nTelefono: ' + this.telefono + '\nCiudad: ' + this.ubicaciones.ciudad + '\nDireccion: ' + this.ubicaciones.direccion + '\n___________________________________';
-   }
-}]
-
-agregar(persona = {
-  id: 3,
-  nombre: 'Andy',
-  apellido: 'Trujillo',
-  telefono: 321456,
-  ubicaciones: {
-    ciudad: 'Armenia',
-    direccion: 'Mz 4 Cs 144',
-  },
-  get datosCompletos(){
-    return 'Contacto N°: '+ this.id + '\nNombre: ' + this.nombre + '\nApellido: ' + this.apellido + 
-    '\nTelefono: ' + this.telefono + '\nCiudad: ' + this.ubicaciones.ciudad + '\nDireccion: ' + this.ubicaciones.direccion + '\n___________________________________';
- }
-})
-
+];
 
 mostrar();
 
+agregar(
+  (contacto[2] = {
+    id: 3,
+    nombre: "Andy",
+    apellido: "Trujillo",
+    telefono: 321456,
+    ubicaciones: {
+      ciudad: "Armenia",
+      direccion: "Mz 4 Cs 144",
+    },
+    get datosCompletos() {
+      return (
+        "Contacto N°: " +
+        this.id +
+        "\nNombre: " +
+        this.nombre +
+        "\nApellido: " +
+        this.apellido +
+        "\nTelefono: " +
+        this.telefono +
+        "\nCiudad: " +
+        this.ubicaciones.ciudad +
+        "\nDireccion: " +
+        this.ubicaciones.direccion +
+        "\n___________________________________"
+      );
+    },
+  })
+);
 
-//console.log(persona.datosCompletos);
-//console.log(contacto[1].datosCompletos);
+mostrar();
+eliminar(2);
+mostrar();
